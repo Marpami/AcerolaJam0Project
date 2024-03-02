8e25d93bf2905ea52de5a6b8c071c1119d6d8f69
Marpami <110490076+Marpami@users.noreply.github.com>
2024-03-02 14:35:06 +0200

    Level Sketch
    - Made a sketch of the level using TilMap
    - Adjusted the softness of ropes pin joints to be a bit less 4.0 -> 3.0

--- Modules/Assets/Player/Dog/dog.gd
+++ Modules/Assets/Player/Dog/dog.gd
@@ -1,50 +1,43 @@
 extends RigidBody2D
 
+
+
+
+
 var wait_time_move : float = 0.5
 var waiting : bool = false
 var can_move : bool = true
+var on_ground : bool = false
+
+
+
+
+
 
 
 func _input(event):
 	# Wait for the movement cooldown to go down
 	if waiting == true:
 		return
-
-
-	# While flying use this method of movement
-	if GlobalVar.is_dog_flying == true:
-		# Movement
-		if event.is_action_pressed("down"):
-			waiting = true
-			movement_timer()
-			linear_velocity = Vector2(0, 60)
+	# Movement
+	if event.is_action_pressed("down"):
+		waiting = true
+		movement_timer()
+		linear_velocity = Vector2(0, 60)
 		
-		if event.is_action_pressed("left"):
-			waiting = true
-			linear_velocity = Vector2(30, 0)
-			await get_tree().create_timer(0.3).timeout
-			linear_velocity = Vector2(-50, 0)
-			movement_timer()
+	if event.is_action_pressed("left"):
+		waiting = true
+		linear_velocity = Vector2(30, 0)
+		await get_tree().create_timer(0.3).timeout
+		linear_velocity = Vector2(-50, 0)
+		movement_timer()
 		
-		if event.is_action_pressed("right"):
-			waiting = true
-			linear_velocity = Vector2(-30, 0)
-			await get_tree().create_timer(0.3).timeout
-			linear_velocity = Vector2(50, 0)
-			movement_timer()
-		return
-	
-	if GlobalVar.is_dog_flying == false:
-		if event.is_action_pressed("left"):
-			waiting = true
-			linear_velocity = Vector2(-50, 0)
-			movement_timer()
-		
-		if event.is_action_pressed("right"):
-			waiting = true
-			linear_velocity = Vector2(50, 0)
-			movement_timer()
-
+	if event.is_action_pressed("right"):
+		waiting = true
+		linear_velocity = Vector2(-30, 0)
+		await get_tree().create_timer(0.3).timeout
+		linear_velocity = Vector2(50, 0)
+		movement_timer()
 
 
 func movement_timer():
